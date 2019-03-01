a69b5fd94a3248133c9e5fd2a3efdaa0d259e52c
GeorgWolf81 <48139716+GeorgWolf81@users.noreply.github.com>
2019-03-01 23:43:40 +0100

    Add files via upload

---
+++ t2.php
@@ -0,0 +1,16 @@
+<?php
+	$txt1 = "Learn PHP";
+	$txt2 = "W3Schools.com";
+	$x = 5;
+	$y = 4;
+
+	echo "<h1>Hello, PHP! h1</h1>";
+	echo "<h2>Hello, PHP! h2</h2>";
+	echo "<h3>Hello, PHP! h3</h3>";
+	echo "<h4>Hello, PHP! h4</h4>";
+	echo "<h5>Hello, PHP! h5</h5>";
+
+	print "<h3>" . $txt1 . "</h3>";
+	print "Study PHP at " . $txt2 . "<BR>";
+	print $x + $y;
+?> 
